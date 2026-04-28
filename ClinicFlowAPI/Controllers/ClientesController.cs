ae70cc8382c423920efd8a80cd7bd516f07abb14
Li-desing <lleiva10893@ufide.ac.cr>
2026-04-28 16:32:55 -0600

    Agregar archivos de proyecto.

---
+++ ClinicFlowAPI/Controllers/ClientesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClinicFlowAPI.Models;
+
+namespace ClinicFlowAPI.Controllers
+{
+    //convierte la clase en API
+    [ApiController] 
+
+    //cuando ingresen a api clientes ejecute este controller
+    [Route("api/[controller]")]
+
+    public class ClientesController : ControllerBase
+    {
+
+        private static List<Cliente> clientes = new List<Cliente>
+        {
+            new Cliente
+            {
+                Id = 1,
+                Nombre = "Jacqueline",
+                PrimerApellido = "Vargas",
+                SegundoApellido = "Mora",
+                Email = "jacqueline@gmail.com",
+                Telefono = "8888-1111"
+            }
+        };
+
+        /// Obtiene la lista completa de clientes registrados.
+        [HttpGet]
+        public IActionResult ObtenerClientes()
+        {
+            return Ok(clientes);
+        }
+
+        /// Registra un nuevo cliente en la lista.
+        [HttpPost]
+        public IActionResult CrearCliente([FromBody] Cliente cliente)
+        {
+            cliente.Id = clientes.Count + 1;
+
+            clientes.Add(cliente);
+
+            return Ok(cliente);
+        }
+    }
+}
---
+++ ClinicFlowAPI/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿namespace ClinicFlowAPI.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string PrimerApellido { get; set; } = string.Empty;
+
+        public string SegundoApellido { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Telefono { get; set; } = string.Empty;
+    }
+    //empty da un valor inicial vacío
+}
